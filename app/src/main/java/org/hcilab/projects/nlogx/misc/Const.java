f88d152b15247abd990ba848ba7436589c24beb5
Mohd Arif <mohdarif8299gmail.com>
2019-08-10 18:40:17 +0530

    Android-notifcation-log-master

---
+++ app/src/main/java/org/hcilab/projects/nlogx/misc/Const.java
@@ -0,0 +1,27 @@
+package org.hcilab.projects.nlogx.misc;
+
+import org.hcilab.projects.nlogx.BuildConfig;
+
+public class Const {
+
+	public static final boolean DEBUG = BuildConfig.DEBUG;
+	public static final long VERSION  = BuildConfig.VERSION_CODE;
+
+	// Feature flags
+	public static final boolean ENABLE_ACTIVITY_RECOGNITION = true;
+	public static final boolean ENABLE_LOCATION_SERVICE     = true;
+
+	// Preferences shown in the UI
+	public static final String PREF_STATUS  = "pref_status";
+	public static final String PREF_BROWSE  = "pref_browse";
+	public static final String PREF_TEXT    = "pref_text";
+	public static final String PREF_ONGOING = "pref_ongoing";
+	public static final String PREF_ABOUT   = "pref_about";
+	public static final String PREF_VERSION = "pref_version";
+	public static final String PREF_THEME = "pref_theme";
+
+	// Preferences not shown in the UI
+	public static final String PREF_LAST_ACTIVITY  = "pref_last_activity";
+	public static final String PREF_LAST_LOCATION  = "pref_last_location";
+
+}
